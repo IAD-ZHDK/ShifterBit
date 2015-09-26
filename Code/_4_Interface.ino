9a7db35b685ff2c774889dde4731895582994f56
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-09-26 16:00:40 +0200

    added input smoothing

--- Code/_4_Interface.ino
+++ Code/_4_Interface.ino
@@ -1,21 +1,27 @@
 #define INTERFACE_INTERVAL 200
 #define INTERACE_IN A0
 #define INTERFACE_OUT 16
+#define INTERFACE_HISTORY 20
 
-int interface_input = 0;
+int interface_input_history[INTERFACE_HISTORY];
+int interface_last_input = 0;
 long long interface_last_read = 0;
 
 void interface_setup() {
   pinMode(OUTPUT, INTERFACE_OUT);
   interface_out(0);
+
+  for(int i=0; i < INTERFACE_HISTORY; i++) {
+    interface_input_history[i] = 0;
+  }
 }
 
 void interface_loop() {
-  int v = analogRead(INTERACE_IN);
+  int v = interface_filter(analogRead(INTERACE_IN));
 
-  if(v != interface_input) {
+  if(v != interface_last_input) {
     if(millis() - INTERFACE_INTERVAL > interface_last_read) {
-      interface_input = v;
+      interface_last_input = v;
       interface_last_read = millis();
 
       if(config_publish) {
@@ -30,3 +36,17 @@ void interface_out(int v) {
   analogWrite(INTERFACE_OUT, v);
 }
 
+int interface_filter(int v) {
+  for(int i=1; i < INTERFACE_HISTORY; i++) {
+    interface_input_history[i - 1] = interface_input_history[i];
+  }
+  
+  interface_input_history[INTERFACE_HISTORY - 1] = v;
+
+  int sum = 0;
+  for(int j=0; j < INTERFACE_HISTORY; j++) {
+    sum += interface_input_history[j];
+  }
+  
+  return sum / INTERFACE_HISTORY;
+}
